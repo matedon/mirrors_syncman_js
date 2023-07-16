bc12f834cc11d7ab36d11e97e1ef72b19c713d00
matedon <matedon@gmail.com>
2023-07-16 13:07:22 +0200

    backend rename smb

--- server.js
+++ server.js
@@ -10,7 +10,14 @@ app.use(express.json())
 app.use(express.urlencoded())
 let server = app.listen(8089)
 
-function strDiff(ai, bi) {
+function reverseString(str) {
+    return str.split('').reverse().join('')
+}
+function strDiff(ai, bi, reverse) {
+	/**
+	 * The reverse argument should be true if expected difference present in the end of the strings!
+	 * Example: ai = "foobar", bi = "foobar-old"
+	 */
 	let i = 0
 	let j = 0
 	let res = ''
@@ -22,6 +29,10 @@ function strDiff(ai, bi) {
 		a = ai
 		b = bi
 	}
+	if (reverse == true) {
+		a = reverseString(a)
+		b = reverseString(b)
+	}
 	while (j < b.length) {
 		if (a[i] != b[j] || i == a.length) {
 			res += b[j]
@@ -29,6 +40,9 @@ function strDiff(ai, bi) {
 			i++
 		}
 		j++
+	}
+	if (reverse == true) {
+		res = reverseString(res)
 	}
 	return res
 }
@@ -130,6 +144,13 @@ app.post('/files', function (req, res) {
 		})
 	})
 })
+const fnRemoveTrailingSlashes = function (str) {
+	if (str[0] == '\\' || str[0] == '/') {
+		// Remove trailing slashes if first character is "\" or "/"
+		str = str.slice(1)
+	}
+	return str
+}
 app.post('/smb', function (req, res) {
 	let problem = false
 	const resFiles = []
@@ -154,12 +175,8 @@ app.post('/smb', function (req, res) {
 		password: req.body.password,
 		autoClose: true
 	})
-	let subdir = strDiff(req.body.share, req.body.open)
-	if (subdir[0] == '\\' || subdir[0] == '/') {
-		//Remove shashes if first character is "\" or "/"
-		subdir = subdir.slice(1)
-	}
-	smb2Client.readdir(subdir, { stats: true }, function(err, data) {
+	let subdir = fnRemoveTrailingSlashes(strDiff(req.body.share, req.body.open, true))
+	smb2Client.readdir(subdir, { stats: true }, function (err, data) {
 		if (err) {
 			problem = 'Unable to scan network: ' + err
 			console.log("Error (readdir):\n", err)
@@ -184,8 +201,9 @@ app.post('/smb', function (req, res) {
 	})
 })
 app.post('/rename-row', function (req, res) {
+	let problem = false
 	if (req.body.col.type == 'files') {
-		const dir = strDiff(req.body.oldRow.path, req.body.oldRow.name)
+		const dir = strDiff(req.body.oldRow.path, req.body.oldRow.name, true)
 		const newPath = dir + req.body.newRow.name
 		console.log(dir, req.body.newRow.name, newPath)
 		const newRow = _.extend({}, req.body.oldRow, {
@@ -194,10 +212,39 @@ app.post('/rename-row', function (req, res) {
 		})
 		fs.rename(req.body.oldRow.path, newPath, function () {
 			res.send({
-				'row': newRow
+				'row': newRow,
+				'problem': problem
 			})
 		})
 	} else if (req.body.col.type == 'smb') {
-		// TODO: rename smb
+		const smb2Client = new SMB2({
+			share: req.body.col.share,
+			domain: req.body.col.domain,
+			username: req.body.col.username,
+			password: req.body.col.password,
+			autoClose: true
+		})
+		// \\192.168.0.30\m\!- A_TUDASTAR_TEMP\E-BOOK (-) \\192.168.0.30\m (=) \!- A_TUDASTAR_TEMP\E-BOOK
+		let dir1 = strDiff(req.body.oldRow.path, req.body.col.share, true)
+		// \!- A_TUDASTAR_TEMP\E-BOOK (=) !- A_TUDASTAR_TEMP\E-BOOK
+		dir1 = fnRemoveTrailingSlashes(dir1)
+		// !- A_TUDASTAR_TEMP\E-BOOK (-) E-BOOK (=) !- A_TUDASTAR_TEMP\
+		let dir2 = strDiff(dir1, req.body.oldRow.name, true)
+		// !- A_TUDASTAR_TEMP\ (+) E-BOOK Archive (=) !- A_TUDASTAR_TEMP\E-BOOK Archive
+		let dir3 = dir2 + req.body.newRow.name
+		console.log('rename', dir1, dir2, dir3)
+		smb2Client.rename(dir1, dir3, { replace: true }, function (err) {
+			if (err) {
+				problem = 'Unable to scan network: ' + err
+				console.log("Error (rename):\n", err)
+			}
+			res.send({
+				'problem': problem,
+				'row': _.extend({}, req.body.oldRow, {
+					'name': req.body.newRow.name,
+					'path': req.body.col.share + '\\' + dir3
+				})
+			})
+		})
 	}
 })
